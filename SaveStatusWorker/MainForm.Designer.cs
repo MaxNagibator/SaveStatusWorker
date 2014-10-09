24c7486b1d9f02df8ed6efaabe0524e60fd1f3a2
maxnagibator <maxnagibator@gmail.com>
2014-10-10 00:23:44 +0700

    add timer

--- SaveStatusWorker/MainForm.Designer.cs
+++ SaveStatusWorker/MainForm.Designer.cs
@@ -28,7 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.uiStartButton = new System.Windows.Forms.Button();
+            this.uiStopButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // textBox1
@@ -38,12 +42,39 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(347, 247);
             this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "300000";
+            // 
+            // uiStartButton
+            // 
+            this.uiStartButton.Location = new System.Drawing.Point(365, 10);
+            this.uiStartButton.Name = "uiStartButton";
+            this.uiStartButton.Size = new System.Drawing.Size(75, 23);
+            this.uiStartButton.TabIndex = 1;
+            this.uiStartButton.Text = "button1";
+            this.uiStartButton.UseVisualStyleBackColor = true;
+            this.uiStartButton.Click += new System.EventHandler(this.uiStartButton_Click);
+            // 
+            // uiStopButton
+            // 
+            this.uiStopButton.Location = new System.Drawing.Point(365, 39);
+            this.uiStopButton.Name = "uiStopButton";
+            this.uiStopButton.Size = new System.Drawing.Size(75, 23);
+            this.uiStopButton.TabIndex = 2;
+            this.uiStopButton.Text = "button2";
+            this.uiStopButton.UseVisualStyleBackColor = true;
+            this.uiStopButton.Click += new System.EventHandler(this.uiStopButton_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(436, 271);
+            this.ClientSize = new System.Drawing.Size(647, 271);
+            this.Controls.Add(this.uiStopButton);
+            this.Controls.Add(this.uiStartButton);
             this.Controls.Add(this.textBox1);
             this.Name = "MainForm";
             this.Text = "Form1";
@@ -56,6 +87,9 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button uiStartButton;
+        private System.Windows.Forms.Button uiStopButton;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- SaveStatusWorker/MainForm.cs
+++ SaveStatusWorker/MainForm.cs
@@ -16,46 +16,9 @@ namespace SaveStatusWorker
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-
-            var settings = GetDocumentClassesFromXdocument(XDocument.Load("settings.txt"));
-
-            var vk = new VkApi();
-            Settings scope = Settings.All; // Права доступа приложения
-            vk.Authorize(settings.AppId, settings.Email, settings.Pass, scope);
-            var x = ProfileFields.Status | ProfileFields.LastName | ProfileFields.FirstName;
-            var frs = vk.Friends.Get((long)vk.UserId, x);
-            using (var context = new DbElements.VkEntities())
-            {
-
-
-                DateTime date = DateTime.Now;
-                foreach (var fr in frs)
-                {
-                    var us = context.User.FirstOrDefault(u => u.Id == fr.Id);
-                    if(us== null)
-                    {
-                        context.User.Add(new DbElements.User()
-                                             {
-                                                 Id = fr.Id,
-                                                 LastName = fr.LastName,
-                                                 FirstName = fr.FirstName
-                                             });
-                    }
-                    var lastStatus = context.Status.Where(s => s.UserId == fr.Id).OrderByDescending(s => s.Date).FirstOrDefault();
-                    if (lastStatus == null || lastStatus.Text != fr.Status)
-                    {
-                        var status = new DbElements.Status();
-                        status.Text = fr.Status;
-                        status.UserId = fr.Id;
-                        status.Date = date;
-                        context.Status.Add(status);
-                    }
-                }
-                context.SaveChanges();
-            }
         }
 
-        private static WorkerSettings GetDocumentClassesFromXdocument(XDocument xDocument)
+        private static WorkerSettings GetSettings(XDocument xDocument)
         {
             var setting = new WorkerSettings();
             if (xDocument.Root == null)
@@ -85,6 +48,63 @@ namespace SaveStatusWorker
                 setting.UserId = Convert.ToInt32(userId.Value);
             }
             return setting;
+        }
+
+        private void uiStartButton_Click(object sender, EventArgs e)
+        {
+            timer1.Interval = Convert.ToInt32(textBox1.Text);
+            timer1.Start();
+            
+        }
+
+        private void uiStopButton_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            GetValue();
+        }
+
+        private void GetValue()
+        {
+            Text += "!";
+            var settings = GetSettings(XDocument.Load("settings.txt"));
+
+            var vk = new VkApi();
+            Settings scope = Settings.All; // Права доступа приложения
+            vk.Authorize(settings.AppId, settings.Email, settings.Pass, scope);
+            var x = ProfileFields.Status | ProfileFields.LastName | ProfileFields.FirstName;
+            var frs = vk.Friends.Get((long)vk.UserId, x);
+            using (var context = new DbElements.VkEntities())
+            {
+                DateTime date = DateTime.Now;
+                foreach (var fr in frs)
+                {
+                    var us = context.User.FirstOrDefault(u => u.Id == fr.Id);
+                    if (us == null)
+                    {
+                        context.User.Add(new DbElements.User
+                        {
+                            Id = fr.Id,
+                            LastName = fr.LastName,
+                            FirstName = fr.FirstName
+                        });
+                    }
+                    var lastStatus =
+                        context.Status.Where(s => s.UserId == fr.Id).OrderByDescending(s => s.Date).FirstOrDefault();
+                    if (lastStatus == null || lastStatus.Text != fr.Status)
+                    {
+                        var status = new DbElements.Status();
+                        status.Text = fr.Status;
+                        status.UserId = fr.Id;
+                        status.Date = date;
+                        context.Status.Add(status);
+                    }
+                }
+                context.SaveChanges();
+            }
         }
     }
 }
