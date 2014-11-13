11865119feae6d0c2cb27511e954ff8bcfb7c0fa
maxnagibator <maxnagibator@gmail.com>
2014-11-13 22:45:33 +0600

    work work

--- SaveStatusWorker/MainForm.Designer.cs
+++ SaveStatusWorker/MainForm.Designer.cs
@@ -29,40 +29,44 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.uiTimerIntervalTextBox = new System.Windows.Forms.TextBox();
             this.uiStartButton = new System.Windows.Forms.Button();
             this.uiStopButton = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.uiShowStatusesButton = new System.Windows.Forms.Button();
+            this.uiOutTextBox = new System.Windows.Forms.TextBox();
+            this.uiMessageLabel = new System.Windows.Forms.Label();
+            this.uiGetFriendsButton = new System.Windows.Forms.Button();
+            this.uiFriendsCombobox = new System.Windows.Forms.ComboBox();
+            this.uiAlbumComboBox = new System.Windows.Forms.ComboBox();
+            this.uiGetAlbumsButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox1
+            // uiTimerIntervalTextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(347, 247);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.Text = "300000";
+            this.uiTimerIntervalTextBox.Location = new System.Drawing.Point(12, 12);
+            this.uiTimerIntervalTextBox.Name = "uiTimerIntervalTextBox";
+            this.uiTimerIntervalTextBox.Size = new System.Drawing.Size(51, 20);
+            this.uiTimerIntervalTextBox.TabIndex = 0;
+            this.uiTimerIntervalTextBox.Text = "300";
             // 
             // uiStartButton
             // 
-            this.uiStartButton.Location = new System.Drawing.Point(365, 10);
+            this.uiStartButton.Location = new System.Drawing.Point(69, 9);
             this.uiStartButton.Name = "uiStartButton";
             this.uiStartButton.Size = new System.Drawing.Size(75, 23);
             this.uiStartButton.TabIndex = 1;
-            this.uiStartButton.Text = "button1";
+            this.uiStartButton.Text = "start";
             this.uiStartButton.UseVisualStyleBackColor = true;
             this.uiStartButton.Click += new System.EventHandler(this.uiStartButton_Click);
             // 
             // uiStopButton
             // 
-            this.uiStopButton.Location = new System.Drawing.Point(365, 39);
+            this.uiStopButton.Location = new System.Drawing.Point(150, 9);
             this.uiStopButton.Name = "uiStopButton";
             this.uiStopButton.Size = new System.Drawing.Size(75, 23);
             this.uiStopButton.TabIndex = 2;
-            this.uiStopButton.Text = "button2";
+            this.uiStopButton.Text = "stop";
             this.uiStopButton.UseVisualStyleBackColor = true;
             this.uiStopButton.Click += new System.EventHandler(this.uiStopButton_Click);
             // 
@@ -70,35 +74,86 @@
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // button1
+            // uiShowStatusesButton
             // 
-            this.button1.Location = new System.Drawing.Point(376, 83);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.uiShowStatusesButton.Location = new System.Drawing.Point(12, 51);
+            this.uiShowStatusesButton.Name = "uiShowStatusesButton";
+            this.uiShowStatusesButton.Size = new System.Drawing.Size(213, 23);
+            this.uiShowStatusesButton.TabIndex = 3;
+            this.uiShowStatusesButton.Text = "Показать последние статусы";
+            this.uiShowStatusesButton.UseVisualStyleBackColor = true;
+            this.uiShowStatusesButton.Click += new System.EventHandler(this.uiShowStatusesButton_Click);
             // 
-            // textBox2
+            // uiOutTextBox
             // 
-            this.textBox2.Location = new System.Drawing.Point(406, 131);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(347, 247);
-            this.textBox2.TabIndex = 4;
-            this.textBox2.Text = "300000";
+            this.uiOutTextBox.Location = new System.Drawing.Point(12, 80);
+            this.uiOutTextBox.Multiline = true;
+            this.uiOutTextBox.Name = "uiOutTextBox";
+            this.uiOutTextBox.Size = new System.Drawing.Size(347, 200);
+            this.uiOutTextBox.TabIndex = 4;
+            // 
+            // uiMessageLabel
+            // 
+            this.uiMessageLabel.AutoSize = true;
+            this.uiMessageLabel.Location = new System.Drawing.Point(12, 35);
+            this.uiMessageLabel.Name = "uiMessageLabel";
+            this.uiMessageLabel.Size = new System.Drawing.Size(313, 13);
+            this.uiMessageLabel.TabIndex = 5;
+            this.uiMessageLabel.Text = "Жми старт и начнется получение статусов раз в 300 секунд";
+            // 
+            // uiGetFriendsButton
+            // 
+            this.uiGetFriendsButton.Location = new System.Drawing.Point(364, 9);
+            this.uiGetFriendsButton.Name = "uiGetFriendsButton";
+            this.uiGetFriendsButton.Size = new System.Drawing.Size(84, 23);
+            this.uiGetFriendsButton.TabIndex = 6;
+            this.uiGetFriendsButton.Text = "Get friends";
+            this.uiGetFriendsButton.UseVisualStyleBackColor = true;
+            this.uiGetFriendsButton.Click += new System.EventHandler(this.uiGetFriendsButton_Click);
+            // 
+            // uiFriendsCombobox
+            // 
+            this.uiFriendsCombobox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.uiFriendsCombobox.FormattingEnabled = true;
+            this.uiFriendsCombobox.Location = new System.Drawing.Point(454, 11);
+            this.uiFriendsCombobox.Name = "uiFriendsCombobox";
+            this.uiFriendsCombobox.Size = new System.Drawing.Size(402, 21);
+            this.uiFriendsCombobox.TabIndex = 7;
+            // 
+            // uiAlbumComboBox
+            // 
+            this.uiAlbumComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.uiAlbumComboBox.FormattingEnabled = true;
+            this.uiAlbumComboBox.Location = new System.Drawing.Point(454, 40);
+            this.uiAlbumComboBox.Name = "uiAlbumComboBox";
+            this.uiAlbumComboBox.Size = new System.Drawing.Size(402, 21);
+            this.uiAlbumComboBox.TabIndex = 9;
+            // 
+            // uiGetAlbumsButton
+            // 
+            this.uiGetAlbumsButton.Location = new System.Drawing.Point(364, 38);
+            this.uiGetAlbumsButton.Name = "uiGetAlbumsButton";
+            this.uiGetAlbumsButton.Size = new System.Drawing.Size(84, 23);
+            this.uiGetAlbumsButton.TabIndex = 8;
+            this.uiGetAlbumsButton.Text = "Get albums";
+            this.uiGetAlbumsButton.UseVisualStyleBackColor = true;
+            this.uiGetAlbumsButton.Click += new System.EventHandler(this.uiGetAlbumsButton_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(868, 419);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(868, 297);
+            this.Controls.Add(this.uiAlbumComboBox);
+            this.Controls.Add(this.uiGetAlbumsButton);
+            this.Controls.Add(this.uiFriendsCombobox);
+            this.Controls.Add(this.uiGetFriendsButton);
+            this.Controls.Add(this.uiMessageLabel);
+            this.Controls.Add(this.uiOutTextBox);
+            this.Controls.Add(this.uiShowStatusesButton);
             this.Controls.Add(this.uiStopButton);
             this.Controls.Add(this.uiStartButton);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.uiTimerIntervalTextBox);
             this.Name = "MainForm";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.MainForm_Load);
@@ -109,12 +164,17 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox uiTimerIntervalTextBox;
         private System.Windows.Forms.Button uiStartButton;
         private System.Windows.Forms.Button uiStopButton;
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button uiShowStatusesButton;
+        private System.Windows.Forms.TextBox uiOutTextBox;
+        private System.Windows.Forms.Label uiMessageLabel;
+        private System.Windows.Forms.Button uiGetFriendsButton;
+        private System.Windows.Forms.ComboBox uiFriendsCombobox;
+        private System.Windows.Forms.ComboBox uiAlbumComboBox;
+        private System.Windows.Forms.Button uiGetAlbumsButton;
     }
 }
 
--- SaveStatusWorker/MainForm.cs
+++ SaveStatusWorker/MainForm.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Windows.Forms;
 using System.Xml.Linq;
 using VkNet;
@@ -12,6 +14,10 @@ namespace SaveStatusWorker
 {
     public partial class MainForm : Form
     {
+        private int _totalRequest;
+        private int _successRequest;
+        private int _failRequest;
+
         public MainForm()
         {
             InitializeComponent();
@@ -19,8 +25,89 @@ namespace SaveStatusWorker
 
         private void MainForm_Load(object sender, EventArgs e)
         {
+            _totalRequest = 0;
+            _successRequest = 0;
+            _failRequest = 0;
         }
 
+        private void uiStartButton_Click(object sender, EventArgs e)
+        {
+            GetStatuses();
+            timer1.Interval = Convert.ToInt32(uiTimerIntervalTextBox.Text)*1000;
+            timer1.Start();
+        }
+
+        private void uiStopButton_Click(object sender, EventArgs e)
+        {
+            uiMessageLabel.Text = "stoped";
+            timer1.Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            GetStatuses();
+        }
+
+        private void GetStatuses()
+        {
+            _totalRequest++;
+            try
+            {
+                var frs = VkWorker.GetFriends();
+                DbStatusWorker.SaveStatuses(frs);
+                _successRequest++;
+            }
+            catch (Exception)
+            {
+                _failRequest++;
+            }
+            uiMessageLabel.Text = "total:" + _totalRequest + " success:" + _successRequest + " fail:" + _failRequest;
+        }
+
+
+        private void uiShowStatusesButton_Click(object sender, EventArgs e)
+        {
+            var statuses = DbStatusWorker.GetStatuses();
+            foreach (var text in statuses)
+            {
+                uiOutTextBox.Text += text + Environment.NewLine;
+            }
+        }
+
+        private void uiGetFriendsButton_Click(object sender, EventArgs e)
+        {
+            var frs = VkWorker.GetFriends();
+            var dict = new List<KeyValuePair<long, string>>();
+            foreach (var fr in frs.OrderBy(f=>f.LastName).ThenBy(f=>f.FirstName))
+            {
+                dict.Add(new KeyValuePair<long, string>(fr.Id, fr.LastName + " " + fr.FirstName));
+            }
+            uiFriendsCombobox.DisplayMember = "Value";
+            uiFriendsCombobox.ValueMember = "Key";
+            uiFriendsCombobox.DataSource = dict;
+        }
+
+        private void uiGetAlbumsButton_Click(object sender, EventArgs e)
+        {
+            long? selectedId = null;
+            if (uiFriendsCombobox.SelectedItem != null)
+            {
+                selectedId = ((User) uiFriendsCombobox.SelectedItem).Id;
+            }
+                var albms = VkWorker.GetAlbums(selectedId);
+            var dict = new List<KeyValuePair<long, string>>();
+            foreach (var album in albms.OrderBy(f => f.Title))
+            {
+                dict.Add(new KeyValuePair<long, string>(album.Id, album.Title + "(" + album.Size+")"));
+            }
+            uiFriendsCombobox.DisplayMember = "Value";
+            uiFriendsCombobox.ValueMember = "Key";
+            uiFriendsCombobox.DataSource = dict; 
+           
+        }
+    }
+    public class VkWorker
+    {
         private static WorkerSettings GetSettings(XDocument xDocument)
         {
             var setting = new WorkerSettings();
@@ -53,57 +140,32 @@ namespace SaveStatusWorker
             return setting;
         }
 
-        private void uiStartButton_Click(object sender, EventArgs e)
+        public static ReadOnlyCollection<User> GetFriends()
         {
-            timer1.Interval = Convert.ToInt32(textBox1.Text);
-            timer1.Start();
-            
-        }
-
-        private void uiStopButton_Click(object sender, EventArgs e)
-        {
-            timer1.Stop();
-        }
-
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            GetValue();
-        }
-
-        private void GetValue()
-        {
-            Text += "!";
-            var settings = GetSettings(XDocument.Load("settings.txt"));
-
-            var vk = new VkApi();
-            Settings scope = Settings.All; // Права доступа приложения
-            vk.Authorize(settings.AppId, settings.Email, settings.Pass, scope);
+            var vk = GetVkApi();
             var x = ProfileFields.Status | ProfileFields.LastName | ProfileFields.FirstName;
             var frs = vk.Friends.Get((long)vk.UserId, x);
-            Class1.SaveStatuses(frs);
+            return frs;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        public static ReadOnlyCollection<PhotoAlbum> GetAlbums(long? userId)
         {
-            GetValue();
-            //Text += "!";
-            //var settings = GetSettings(XDocument.Load("settings.txt"));
+            var vk = GetVkApi();
+            var frs = vk.Photo.GetAlbums(userId);
+            return frs;
+        }
 
-            //var vk = new VkApi();
-            //Settings scope = Settings.All; // Права доступа приложения
-            //vk.Authorize(settings.AppId, settings.Email, settings.Pass, scope);
-            //var x = ProfileFields.Status | ProfileFields.LastName | ProfileFields.FirstName;
-            //var frs = vk.Friends.Get((long)vk.UserId, x);
-            //var x3 = Class1.SaveStatuses(frs);
-            var x3 = Class1.GetStatuses();
-            foreach (var text in x3)
-            {
-                textBox2.Text += text + Environment.NewLine;
-            }
+        private static VkApi GetVkApi()
+        {
+            var settings = GetSettings(XDocument.Load("settings.txt"));
+            var vk = new VkApi();
+            Settings scope = Settings.All;
+            vk.Authorize(settings.AppId, settings.Email, settings.Pass, scope);
+            return vk;
         }
     }
 
-    internal class Class1
+    public class DbStatusWorker
     {
         public static void SaveStatuses(ReadOnlyCollection<User> frs)
         {
@@ -116,11 +178,11 @@ namespace SaveStatusWorker
                     if (us == null)
                     {
                         context.User.Add(new DbElements.User
-                        {
-                            Id = fr.Id,
-                            LastName = fr.LastName,
-                            FirstName = fr.FirstName
-                        });
+                                             {
+                                                 Id = fr.Id,
+                                                 LastName = fr.LastName,
+                                                 FirstName = fr.FirstName
+                                             });
                     }
                     var lastStatus =
                         context.Status.Where(s => s.UserId == fr.Id).OrderByDescending(s => s.Date).FirstOrDefault();
@@ -140,9 +202,11 @@ namespace SaveStatusWorker
         public static List<string> GetStatuses()
         {
             using (var context = new DbElements.Entities1())
-             {
-                 return context.Status.Select(s => s.Text).Take(10).ToList();
-             }
+            {
+                return context.Status
+                    .OrderByDescending(s => s.Date)
+                    .Select(s => s.Text).Take(10).ToList();
+            }
         }
     }
 }
